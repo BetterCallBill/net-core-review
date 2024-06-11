c3292668a46a337966c0de794fd5f5ee2fd0aa3f
Billy <billy.wang.aus@gmail.com>
2024-06-11 11:21:12 +1000

    add person

--- src/ServiceContracts/DTO/PersonAddRequest.cs
+++ src/ServiceContracts/DTO/PersonAddRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Entities;
 using ServiceContracts.DTO.Enums;
 
@@ -5,8 +6,13 @@ namespace ServiceContracts.DTO
 {
     public class PersonAddRequest
     {
+        [Required(ErrorMessage = "Person Name can't be blank")]
         public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Email value should be a valid email")]
         public string? Email { get; set; }
+        
         public DateTime? DateOfBirth { get; set; }
         public GenderOptions? Gender { get; set; }
         public Guid? CountryID { get; set; }
@@ -19,7 +25,16 @@ namespace ServiceContracts.DTO
         /// <returns></returns>
         public Person ToPerson()
         {
-            return new Person() { PersonName = PersonName, Email = Email, DateOfBirth = DateOfBirth, Gender = Gender.ToString(), Address = Address, CountryID = CountryID, ReceiveNewsLetters = ReceiveNewsLetters };
+            return new Person()
+            {
+                PersonName = PersonName,
+                Email = Email,
+                DateOfBirth = DateOfBirth,
+                Gender = Gender.ToString(),
+                Address = Address,
+                CountryID = CountryID,
+                ReceiveNewsLetters = ReceiveNewsLetters
+            };
         }
     }
 }
