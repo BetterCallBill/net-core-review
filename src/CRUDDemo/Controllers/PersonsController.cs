642d787b004278c5016313a90fc0693a9b90bee8
Billy <billy.wang.aus@gmail.com>
2024-06-12 16:51:25 +1000

    add create page

--- src/CRUDDemo/Controllers/PersonsController.cs
+++ src/CRUDDemo/Controllers/PersonsController.cs
@@ -5,19 +5,22 @@ using ServiceContracts.Enums;
 
 namespace CRUDDemo.Controllers
 {
+    [Route("[controller]/[action]")]
     public class PersonsController : Controller
     {
         //private fields
         private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
 
         //constructor
-        public PersonsController(IPersonsService personsService)
+        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
         {
             _personsService = personsService;
+            _countriesService = countriesService;
         }
 
         [Route("/")]
-        [Route("persons/index")]
+        [HttpGet]
         // string sortBy, SortOrderOptions sortOrder
         public IActionResult Index(
             string searchBy,
@@ -42,8 +45,36 @@ namespace CRUDDemo.Controllers
             List<PersonResponse> sortedPersons = _personsService.GetSortedPersons(persons, sortBy, sortOrder);
             ViewBag.CurrentSortBy = sortBy;
             ViewBag.CurrentSortOrder = sortOrder.ToString();
-            
-            return View(sortedPersons); //Views/Persons/Index.cshtml
+
+            return View(sortedPersons); //Views/Index.cshtml
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<CountryResponse> countries = _countriesService.GetAllCountries();
+            ViewBag.Countries = countries;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(PersonAddRequest? personAddRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<CountryResponse> countries = _countriesService.GetAllCountries();
+                ViewBag.Countries = countries;
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View();
+            }
+
+            //call the service method
+            PersonResponse personResponse = _personsService.AddPerson(personAddRequest);
+
+            //navigate to Index() action method (it makes another get request to "index"
+            return RedirectToAction("Index", "Persons");
         }
     }
 }
