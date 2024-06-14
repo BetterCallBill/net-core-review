eeef1ef48f604cd63f4871bd0609db3f2f6004b2
Billy <billy.wang.aus@gmail.com>
2024-06-14 13:23:30 +1000

    add docker & sql connection

--- src/Entities/PersonsDbContext.cs
+++ src/Entities/PersonsDbContext.cs
@@ -11,11 +11,26 @@ namespace Entities
         public DbSet<Person> Persons { get; set; }
 
         public DbSet<Country> Countries { get; set; }
-        
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Person>().ToTable("Persons");
             modelBuilder.Entity<Country>().ToTable("Countries");
+
+            //Seed to Countries
+            string countriesJson = File.ReadAllText("countries.json");
+            List<Country> countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            foreach (Country country in countries)
+                modelBuilder.Entity<Country>().HasData(country);
+
+
+            //Seed to Persons
+            string personsJson = File.ReadAllText("persons.json");
+            List<Person> persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+            foreach (Person person in persons)
+                modelBuilder.Entity<Person>().HasData(person);
         }
     }
 }
